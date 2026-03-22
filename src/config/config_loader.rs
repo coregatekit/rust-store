891ddca73b274dbde5ea54e059b1e070bf366111
Joe Kim <joesm.genie@gmail.com>
2026-03-22 12:39:49 +0700

    add function get stage

--- src/config/config_loader.rs
+++ src/config/config_loader.rs
@@ -1,6 +1,6 @@
-use anyhow::Result;
+use anyhow::{Ok, Result};
 
-use crate::config::config_model::{Database, DotEnvyConfig, Server};
+use crate::config::{config_model::{Database, DotEnvyConfig, Server}, stage::Stage};
 
 pub fn load() -> Result<DotEnvyConfig> {
     dotenvy::dotenv().ok();
@@ -22,4 +22,12 @@ pub fn load() -> Result<DotEnvyConfig> {
     };
 
     Ok(DotEnvyConfig { server, database })
+}
+
+pub fn get_stage() -> Stage {
+    dotenvy::dotenv().ok();
+
+    let stage_str = std::env::var("STAGE").unwrap_or("".to_string());
+
+    Stage::try_from(&stage_str).unwrap_or_default()
 }
