f3230e7b83f553ff47f7d29fd0adfbb0193de607
Joe Kim <joesm.genie@gmail.com>
2026-03-22 14:56:56 +0700

    start server in main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
+use std::sync::Arc;
+
+use rust_store::{config::config_loader, infrastructure::axum_http::http_serve::start};
+use tracing::{error, info};
+
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt().with_max_level(tracing::Level::DEBUG).init();
 
-    println!("Hello, world!");
+    let dotenvy_env = match config_loader::load() {
+        Ok(env) => env,
+        Err(e) => {
+            error!("Failed to load environment variables: {}", e);
+            std::process::exit(1);
+        } 
+    };
+
+    info!("Environment variables loaded successfully: {:?}", dotenvy_env);
+
+    start(Arc::new(dotenvy_env)).await.expect("Failed to start server");
 }
