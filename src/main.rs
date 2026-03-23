34caa0cddaf4ecf94cdc513d11a69ce97c61ad63
Joe Kim <joesm.genie@gmail.com>
2026-03-23 13:56:38 +0700

    update logging

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,44 @@
 use std::sync::Arc;
 
-use rust_store::{config::config_loader, infrastructure::{axum_http::http_serve::start, postgres::connection::establish_connection}};
+use rust_store::{
+    config::config_loader,
+    infrastructure::{axum_http::http_serve::start, postgres::connection::establish_connection},
+};
 use tracing::{error, info};
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt().with_max_level(tracing::Level::DEBUG).init();
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+
+    info!("Starting Rust Store application...");
+
+    info!("Loading configuration...");
 
     let dotenvy_env = match config_loader::load() {
         Ok(env) => env,
         Err(e) => {
             error!("Failed to load environment variables: {}", e);
             std::process::exit(1);
-        } 
+        }
     };
 
-    info!("Environment variables loaded successfully:"); 
+    info!("Configuration loaded successfully");
+
+    info!("Establishing database connection...");
 
     let postgres_pool = match establish_connection(&dotenvy_env.database.url) {
         Ok(pool) => pool,
         Err(err) => {
             error!("Failed to establish database connection: {err}");
             std::process::exit(1);
-        },
+        }
     };
 
     info!("Database connection established successfully");
 
-    start(Arc::new(dotenvy_env), Arc::new(postgres_pool)).await.expect("Failed to start server");
+    start(Arc::new(dotenvy_env), Arc::new(postgres_pool))
+        .await
+        .expect("Failed to start server");
 }
