64c9ec399f82fa5f822d08d8e2bc93931729ace6
Joe Kim <joesm.genie@gmail.com>
2026-03-23 15:04:28 +0700

    add ProductEntity struct definition in products module

---
+++ src/domain/entities/products.rs
@@ -0,0 +1,16 @@
+use chrono::NaiveDateTime;
+use diesel::prelude::*;
+
+use crate::infrastructure::postgres::schema::products;
+
+#[derive(Debug, Clone, Identifiable, Selectable, Queryable)]
+#[diesel(table_name = products)]
+pub struct ProductEntity {
+    pub id: i32,
+    pub name: String,
+    pub description: Option<String>,
+    pub image_url: Option<String>,
+    pub price: f64,
+    pub created_at: NaiveDateTime,
+    pub updated_at: NaiveDateTime,
+}
